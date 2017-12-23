977c0ae3d66af793aa3a028338f4497b515ea0fd
kmi <kashkinov96@gmail.com>
2017-12-23 14:43:32 +0300

    fixed keeproot

--- main.py
+++ main.py
@@ -32,13 +32,13 @@ def vk_api_request(method_name, parameters=False, token=False):
 
 def get_friends(vk_id, token=ACCESS_TOKEN):
     r = vk_api_request('friends.get','user_id={}&fields=first_name,last_name,photo_50'.format(vk_id), token)
-    # удаляем деактивированные анкеты
     r["items"] = list(filter((lambda x: 'deactivated' not in x.keys()), r['items']))
     return {item['id']: item for item in r['items']}, r['count']
 
 def get_profileInfo(vk_id, token=ACCESS_TOKEN):
-    r = vk_api_request('users.get','user_id={}'.format(vk_id),token)
+    r = vk_api_request('users.get','user_id={}&fields=first_name,last_name,photo_50'.format(vk_id),token)
     return r
+
 
 def get_mutualFriends(vk_id1, vk_id2, token=ACCESS_TOKEN):
     r = vk_api_request('friends.getMutual','source_uid={}&target_uid={}'.format(vk_id1,vk_id2), token)
@@ -47,6 +47,7 @@ def get_mutualFriends(vk_id1, vk_id2, token=ACCESS_TOKEN):
 def add_friends_to_graph(graph,vk_id):
     print("Retrieving id{}'s profile info...".format(vk_id))
     friends_network.add_node(vk_id, **get_profileInfo(vk_id)[0])
+    friends_network.nodes[vk_id]['root'] = 1
     print("Retrieving id{}'s friends and friends of friends...".format(vk_id))
     for f in tqdm.tqdm(get_friends(vk_id)[0].items()):
         graph.add_node(f[0],**f[1])
@@ -71,20 +72,25 @@ def find_communities(graph):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("vkids",nargs='+',help="Vk id or ids to add as root nodes")
-    parser.add_argument("--jsonpath",type=str,help="Load graph from json")
-    parser.add_argument("-k", "--keeproot",action="store_true",help="Keep the root node")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-v","--vkids",nargs='+',help="Vk id or ids to add as root nodes.(may not be used with -j)")
+    group.add_argument("-j","--jsonpath",type=str,help="Load and visualize graph from json.(may not be used with -v)")
+    parser.add_argument("-k", "--keeproot",action="store_true",help="If provided with -v keeps the root node in resulting graph. (ignored with -j)")
     args = parser.parse_args()
+    print(args.keeproot)
     if args.jsonpath is not None :
         with open(args.jsonpath, 'r') as f:
             friends_network = nx.readwrite.node_link_graph(json.load(f))
     else:
+        friends_network = nx.Graph()
         for vid in args.vkids: 
-            friends_network = nx.Graph()
             add_friends_to_graph(friends_network, vid)
-            if args.keeproot:
-                friends_network.remove_node(vid)
+
         find_communities(friends_network)
+        if not args.keeproot:
+            for r in [node for node,data in friends_network.nodes(data=True) if 'root' in data.keys()]:
+                print("Removing id" + r+' ...')
+                friends_network.remove_node(r)
         data = nx.readwrite.json_graph.node_link_data(friends_network)
         with open('friends_network.json', 'w') as f:
             json.dump(data, f, indent=4)
--- server.py
+++ server.py
@@ -1,11 +1,19 @@
 import http.server
 import socketserver
 import webbrowser
+import sys
+
+class ReuseAddrTCPServer(socketserver.TCPServer):
+    allow_reuse_address = True
 
 def run_local_http_server(PORT=8000):
-	Handler = http.server.SimpleHTTPRequestHandler
+    Handler = http.server.SimpleHTTPRequestHandler
 
-	with socketserver.TCPServer(("", PORT), Handler) as httpd:
-	    print("serving at port", PORT)
-	    webbrowser.open('http://localhost:{}/index.html'.format(PORT))
-	    httpd.serve_forever()
+    with ReuseAddrTCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        webbrowser.open('http://localhost:{}/index.html'.format(PORT))
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            httpd.shutdown()
+            sys.exit()
